53dd96adad8c7ad00ac8e7bb4766120ea7259994
webdq <d312697510@126.com>
2021-06-19 00:04:43 +0800

    新增计算等级和升级地点

--- src/utils/replyMessage.js
+++ src/utils/replyMessage.js
@@ -1,7 +1,7 @@
 /*
  * @Author: dengqiang
  * @Date: 2021-06-13 16:32:35
- * @LastEditTime: 2021-06-15 00:17:58
+ * @LastEditTime: 2021-06-19 00:01:10
  * @LastEditors: dengqiang
  * @Description: replyMessage
  */
@@ -15,6 +15,14 @@ const {
   NOTFOUND_MATCH_MESSAGE,
   EMPTY_MESSAGE
 } = require('../../config/bot');
+const {
+  LEVEL,
+  LEVEL_MIN,
+  LEVEL_MAX,
+  LEVEL_TOTAL
+} = require('../../config/level');
+const { Decimal } = require('decimal.js');
+const chinaUnitNum = require('china-unit-num');
 
 const checkMessage = async ({ text, match, talker, room, bot }) => {
   let reply = null;
@@ -22,7 +30,42 @@ const checkMessage = async ({ text, match, talker, room, bot }) => {
   if (match.test.test(text)) {
     switch (match.replyType) {
       case 'text':
-        reply = match.reply;
+        if (match.callType === '经验') {
+          try {
+            const val = text.replace(/,，/g, ',');
+            let [, start, end] = val.split(',');
+            let s = LEVEL_MIN,
+              e = LEVEL_MAX;
+            if (start) {
+              s = Number(start);
+              s = isNaN(s) ? LEVEL_MIN : s;
+              s = Math.min(Math.max(s, LEVEL_MIN), LEVEL_MAX);
+            }
+            if (end) {
+              e = Number(end);
+              e = isNaN(e) ? LEVEL_MAX : e;
+              e = Math.max(Math.min(e, LEVEL_MAX), LEVEL_MIN);
+            }
+            if (start && end && e <= s) {
+              e = e + 1;
+            }
+            if (start && end && s === LEVEL_MIN && e === LEVEL_MAX) {
+              const lv = chinaUnitNum(LEVEL_TOTAL);
+              reply = `${LEVEL_MAX}级 经验约等于：${lv}`;
+            } else {
+              const lvList = LEVEL.slice(s - 1, e - 1);
+              const lvDecimal = lvList.reduce((prev, cur) => {
+                return Decimal.add(prev, cur);
+              }, new Decimal(0));
+              const lv = chinaUnitNum(lvDecimal.toNumber());
+              reply = `${start}-${end}级 经验约等于：${lv}`;
+            }
+          } catch (err) {
+            reply = `(；′⌒”) 太难了，2年级都算不出来`;
+          }
+        } else {
+          reply = match.reply;
+        }
         if (room) {
           mentionList = await replyMentionList({
             match,
